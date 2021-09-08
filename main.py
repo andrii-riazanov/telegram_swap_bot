a952cbe704bffd0dd99817e0534f9fd1cd6a92f3
andrii-riazanov <andrii.riazanov@gmail.com>
2021-09-08 19:01:54 -0400

    Fixed the 'have' regexp

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ swap_bot = telegram.Bot(TOKEN)
 reddit = praw.Reddit(client_id=RCLIENT_ID, client_secret=RCLIENT_SECRET, user_agent="Agent Smith")
 subreddit = reddit.subreddit("hardwareswap")
 
-have = re.compile(r".*\[H\].*(3080\s*[Tt][Ii]|3090).*[W]")
+have = re.compile(r".*\[H\].*(3080\s*[Tt][Ii]|3090).*\[W\]")
 want = re.compile(r".*\[W\].*3070")
 
 time_script_started = datetime.datetime.now()
@@ -29,7 +29,7 @@ for submission in subreddit.stream.submissions():
     title = submission.title
     time = submission.created_utc
     timestamp = datetime.datetime.fromtimestamp(time)
-    if (time_script_started > timestamp) and (time_script_started - timestamp).seconds > 1800:
+    if (time_script_started > timestamp) and (time_script_started - timestamp).seconds > 180:
         continue
     normal_time = timestamp.strftime('%Y-%m-%d %H:%M:%S')
     if have.match(title) or want.match(title):
